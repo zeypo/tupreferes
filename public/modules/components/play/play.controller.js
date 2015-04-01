8f71853892b3737fffedb7ebc510de9f0602d1b8
zeypo <zeypodesign@gmail.com>
2015-04-01 17:46:18 +0200

    ajout du systeme de vote côté front

--- public/modules/components/play/play.controller.js
+++ public/modules/components/play/play.controller.js
@@ -3,21 +3,37 @@
 function playCtrl($scope, playService) {
 
     var _this       = this;
+    $scope.voted     = false;
 
     playService.getRandomStory().success(function(story) {
         _this.story = story.response;
     });
 
     $scope.reload = function() {
+        
+        $scope.voted = false;
+        $scope.error = false;
+        
         playService.getRandomStory().success(function(story) {
             _this.story = story.response;
         });  
     }
 
     $scope.vote = function(vote) {
-        playService.voteForPrefere(vote, _this.story._id).success(function(story) {
-            console.log(story);
-        })
+        playService.voteForPrefere(vote, _this.story._id)
+            .success(function(story) {
+            
+                $scope.voted = true;
+                _this.story = story.response;
+
+            })
+            .error(function(res) {
+                
+                if(res.statusCode === '400') {
+                    $scope.error = res.errors;
+                }
+            
+            });
     }
 }
 
